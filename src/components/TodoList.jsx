42dd8997bdebbf0731cfabbf2f64d7778e9aecc5
Cristofher <Cristofher.rojas@outlook.com>
2025-07-08 19:47:46 -0400

    Final avance clases

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,37 @@
-import {Fragment} from 'react';
+import {Fragment, useRef, useState} from 'react';
+
+import TodoItem from './TodoItem';
+
+import uuid4 from 'uuid4';
 
 const TodoList = () => {
+	const [todos, setTodos] = useState([
+		{id: 1, tarea: 'Estudiar React', estado: false},
+		{id: 2, tarea: 'Hacer la compra', estado: true},
+		{id: 3, tarea: 'Llamar a mamá', estado: false},
+		{id: 4, tarea: 'Estudiar Bases de datos', estado: false},
+		{id: 5, tarea: 'Hacer Ejercicios', estado: true},
+	]);
+
+	const addTask = () => {
+		const tarea = tareaRef.current.value.trim();
+		if (tarea === '') return;
+		/* Entender que hace esta función */
+		setTodos((prevTodos) => {
+			const nuevaTarea = {
+				id: uuid4(),
+				tarea: tarea,
+				estado: false,
+			};
+			return [...prevTodos, nuevaTarea]
+		});
+		tareaRef.current.value = null;
+	};
+
+	/* Definimos el item que capturará el dato de la tarea */
+
+	const tareaRef = useRef();
+
 	return (
 		<Fragment>
 			<h1 className="display-5 my-3">Lista de tareas ✔️😊</h1>
@@ -10,9 +41,10 @@ const TodoList = () => {
 					type="text"
 					placeholder="Agregar tarea"
 					className="form-control"
+					ref={tareaRef}
 				/>
 
-				<button className="btn btn-success ms-2">
+				<button className="btn btn-success ms-2" onClick={addTask}>
 					<i className="bi bi-folder-plus"></i>
 				</button>
 
@@ -20,6 +52,14 @@ const TodoList = () => {
 					<i className="bi bi-trash3"></i>
 				</button>
 			</div>
+
+			{/* Funcion avanzada de JS */}
+
+			<ul className="list-group my-5">
+				{todos.map((todo) => (
+					<TodoItem key={todo.id} todo={todo} />
+				))}
+			</ul>
 		</Fragment>
 	);
 };
